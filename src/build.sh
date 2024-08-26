80af7c152b12fa79087e256951f8dd03e40f124c
Roy Stewart <roythomasstewart@gmail.com>
2024-08-26 07:15:37 -0400

    Change the base image to Ubuntu server

--- src/build.sh
+++ src/build.sh
@@ -7,4 +7,4 @@ build_dir="$current_dir/build"
 mkdir $build_dir
 
 echo "Downloading base Ubuntu image..."
-curl --output "$build_dir/ubuntu-image.iso" "https://releases.ubuntu.com/24.04/ubuntu-24.04-desktop-amd64.iso?_ga=2.166175230.1786488442.1724552639-1459589724.1696672568"
+curl --output "$build_dir/ubuntu-image.iso" "https://releases.ubuntu.com/24.04/ubuntu-24.04-live-server-amd64.iso?_ga=2.140814834.1786488442.1724552639-1459589724.1696672568
